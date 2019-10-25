6f9fbbf8bcf954777f3293be437200df216bf0f3
sjimenez <sjimenez@elconfidencial.com>
2019-10-25 11:29:33 +0200

    drag and drop with one piece!

--- src/index.js
+++ src/index.js
@@ -1,29 +1,53 @@
-function onDragStart(event) {
-    event
-        .dataTransfer
-        .setData('text/plain', event.target.id);
+import { dragMoveListener } from 'utils/dragMoveListener.js';
 
-    event
-        .currentTarget
-        .style
-        .backgroundColor = 'yellow';
-}
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+    // only accept elements matching this CSS selector
+    accept: '#yes-drop',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
 
-function onDragOver(event) {
-    event.preventDefault();
-}
+    // listen for drop related events:
 
-function onDrop(event) {
-    const id = event
-        .dataTransfer
-        .getData('text');
+    ondropactivate: function (event) {
+        // add active dropzone feedback
+        event.target.classList.add('drop-active');
+    },
+    ondragenter: function (event) {
+        var draggableElement = event.relatedTarget;
+        var dropzoneElement = event.target;
 
-    const draggableElement = document.getElementById(id);
-    const dropzone = event.target;
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target');
+        draggableElement.classList.add('can-drop');
+        draggableElement.textContent = 'Dragged in';
+    },
+    ondragleave: function (event) {
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target');
+        event.relatedTarget.classList.remove('can-drop');
+        event.relatedTarget.textContent = 'Dragged out'
+    },
+    ondrop: function (event) {
+        event.relatedTarget.textContent = 'Dropped';
+    },
+    ondropdeactivate: function (event) {
+        // remove active dropzone feedback
+        event.target.classList.remove('drop-active');
+        event.target.classList.remove('drop-target');
+    }
+});
 
-    dropzone.appendChild(draggableElement);
-
-    event
-        .dataTransfer
-        .clearData();
-}
+interact('.drag-drop')
+    .draggable({
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+        autoScroll: true,
+        // dragMoveListener from the dragging demo above
+        onmove: dragMoveListener
+    });
